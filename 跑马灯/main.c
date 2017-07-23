2abe3faacf98a58831a95d28b21f8183b47b57fc
HyperQing <469379004@qq.com>
2017-07-23 17:11:46 +0800

    跑马灯

--- 跑马灯/main.c
+++ 跑马灯/main.c
@@ -1,10 +1,35 @@
 #include <reg52.h>
 
+void delay100ms(void);
+
 void main(){
-    P0 = 0x01;
+    // 用于区分方向
+    unsigned char direction;
+    direction = 0;
+    P0=0x01;
     while(1){
-        if(P0>=0x00){
-        P0=P0<<1;
-           }
+        if(direction == 0){
+            P0=P0<<1;
+            // 已经到左端的情况，则换向
+            if(P0==0x80){
+                direction = 1;
+            }
+        }else{
+            P0=P0>>1;
+            // 已经到右端的情况，则换向
+            if(P0==0x01){
+                direction = 0;
+            }
+        }
+        delay100ms();
     }
+}
+
+
+void delay100ms(void)   //误差 -0.000000000021us
+{
+    unsigned char a,b,c;
+    for(c=35;c>0;c--)
+        for(b=10;b>0;b--)
+            for(a=130;a>0;a--);
 }
